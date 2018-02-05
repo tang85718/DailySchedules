12bfb033570c6681acf76f8d39ac547ff55a2eea
tangxuyao <daulgas.tang@stringstech.co>
2018-02-05 16:54:13 +0800

    修改判断条件代码

--- app/src/main/java/co/stringstech/notice/schedules/BaseSchedule.java
+++ app/src/main/java/co/stringstech/notice/schedules/BaseSchedule.java
@@ -1,6 +1,8 @@
 package co.stringstech.notice.schedules;
 
+import java.util.Calendar;
 import java.util.TimerTask;
+
 import timber.log.Timber;
 
 /**
@@ -16,9 +18,33 @@ public abstract class BaseSchedule extends TimerTask {
     public void execute() {
         Timber.i("starting: %s", getName());
 
-        long diff = Math.abs(System.currentTimeMillis() - getScheduleTime());
-        if (diff >= 10000) {
-            Timber.w("超过计划时间%d秒，不执行计划", diff / 1000);
+        Calendar today = Calendar.getInstance();
+        today.setTimeInMillis(System.currentTimeMillis());
+
+        Calendar latest = Calendar.getInstance();
+        latest.setTimeInMillis(scheduleTime);
+
+        int th = today.get(Calendar.HOUR_OF_DAY);
+        int tm = today.get(Calendar.MINUTE);
+        int ts = today.get(Calendar.SECOND);
+
+        int lh = latest.get(Calendar.HOUR_OF_DAY);
+        int lm = latest.get(Calendar.MINUTE);
+        int ls = latest.get(Calendar.SECOND);
+
+        if (th != lh) {
+            Timber.w("超过计划时间，不执行计划");
+            return;
+        }
+
+        if (tm != lm) {
+            Timber.w("超过计划时间，不执行计划");
+            return;
+        }
+
+        long diff = Math.abs(ts - ls);
+        if (diff >= 10) {
+            Timber.w("超过计划时间%d秒，不执行计划", diff);
             return;
         }
 
